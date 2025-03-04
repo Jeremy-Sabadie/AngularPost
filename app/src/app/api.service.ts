76feae7a672ff41a0125bea1949339574b089a53
Jérémy Sabadie <sabadie.jeremy@gmail.comgit config --global user.name Jeremy>
2025-03-04 16:21:15 +0100

    Get request seting.

--- app/src/app/api.service.ts
+++ app/src/app/api.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  private apiUrl: string = 'https://jsonplaceholder.typicode.com/posts'; // Remplace par l'URL de ton API
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Effectue un appel GET pour récupérer la liste des données
+   * @returns Observable<any> : Liste des données retournées par l'API
+   */
+  getData(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
 }
--- app/src/app/list/list.component.ts
+++ app/src/app/list/list.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-list',
-  imports: [],
-  templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  template: `
+    <ul>
+      <li *ngFor="let item of data">{{ item.API }}</li>
+    </ul>
+  `,
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  data: any[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getData().subscribe(
+      (response) => {
+        if (response && response.entries) {
+          this.data = response.entries; // Alimente la liste avec les données retournées par l'API
+        }
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération des données', error);
+      }
+    );
+  }
 }
